73df43e4df903228207a882e846b41c13fb32905
Thiago Ribeiro <thiago.srib@gmail.com>
2020-08-07 09:34:38 -0300

    feat: create and stylish GiveClasses mobile page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
-import Landing from './src/pages/Landing';
-
 import { AppLoading } from 'expo';
 import {
   Archivo_400Regular,
@@ -13,6 +11,8 @@ import {
   Poppins_400Regular,
   Poppins_600SemiBold,
 } from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
   let [fontLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style='light' />
       </>
     );
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import landingImage from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -8,7 +10,13 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 import styles from './styles';
 
-const Landing: React.FC = () => {
+function Landing() {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClassesPage() {
+    navigate('GiveClasses');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImage} style={styles.banner} />
@@ -19,15 +27,18 @@ const Landing: React.FC = () => {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.buttons, styles.buttonPrimary]}>
+        <RectButton style={[styles.buttons, styles.buttonPrimary]}>
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
-        </TouchableOpacity>
+        </RectButton>
 
-        <TouchableOpacity style={[styles.buttons, styles.buttonSecondary]}>
+        <RectButton
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.buttons, styles.buttonSecondary]}
+        >
           <Image source={giveClassesIcon} />
           <Text style={styles.buttonText}>Dar aulas</Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
       <Text style={styles.totalConnections}>
@@ -35,6 +46,6 @@ const Landing: React.FC = () => {
       </Text>
     </View>
   );
-};
+}
 
 export default Landing;
